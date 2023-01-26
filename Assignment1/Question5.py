63c1890ef79fb24c9362d5e948daf8d5fb24f27f
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-01-25 20:20:35 -0600

    Create Question5.py

---
+++ Assignment1/Question5.py
@@ -0,0 +1,7 @@
+_radius_of_circle_ = 30 #sets radius of circle to 30
+_area_of_circle_ = 3.14 * (_radius_of_circle_**2) #calculates area of circle
+print(_area_of_circle_) #prints area of circle
+_circum_of_circle_ = 3.14 * (_radius_of_circle_ * 2) #calculates circumference of circle
+print(_circum_of_circle_) #prints circumference of circle
+_radius_of_circle_ = int(input("Enter the radius of a circle ")) #gets user input for radius of circle
+print(3.14 * (_radius_of_circle_**2)) #calculates and prints area of circle
