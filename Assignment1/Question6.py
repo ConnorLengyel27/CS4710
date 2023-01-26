ac6424bca69dbaf01696d4a260189f4ace674318
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-01-25 20:24:30 -0600

    Create Question6.py

---
+++ Assignment1/Question6.py
@@ -0,0 +1,3 @@
+string = "I am a teacher and I love to inspire and teach people" #sets string variable
+uniqueWords = set(string.split(' ')) #splits words in string variable and assigns them to a set
+print(uniqueWords) #prints uniqueWords set
