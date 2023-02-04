a03ebe9a39b620761fa9d317be470b4c385b3868
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-02-04 16:21:10 -0600

    Update Question3.py

--- Assignment 2/Question3.py
+++ Assignment 2/Question3.py
@@ -1,4 +1,6 @@
 x = [23, "Python", 23.98] #list storing different data
 print(x) #prints the list
+newList = []
 for i in x: #for loop that iterates through the loop and prints the type of each variable
-    print(type(i))
+    newList.append(type(i))
+print(newList)
