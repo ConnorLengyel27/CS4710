94513bccb817bb11f03a3ebc360f032808988353
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-02-04 16:21:33 -0600

    Update Question5.py

--- Assignment 2/Question5.py
+++ Assignment 2/Question5.py
@@ -4,7 +4,7 @@ uppercase = 0 #variable to store amount of uppercase characters
 for i in userInput: #for loop that iterates through user input
     if i.islower(): #if statement that checks if the character is lowercase
         lowercase += 1 #if the character is lowercase add 1 to lowercase variable
-    else:
+    elif i.isupper():
         uppercase += 1 #if the character is not lowercase add 1 to uppercase variable
 print("No. of upper-case characters: ", str(uppercase)) #print amount of upper-case characters
 print("No. of lower-case characters: ", str(lowercase)) #print amount of lower-case characters
