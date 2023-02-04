11c5aa2af44af05399aea0d33f5554484553f88b
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-02-04 16:16:33 -0600

    Create Question1.py

---
+++ Assignment 2/Question1.py
@@ -0,0 +1,10 @@
+for i in range(6): #for loop that iterates 6 times
+    for j in range(i): #for loops that iterates through the range of i
+        print('*', end=' ') #prints * in line
+    print() #prints new line
+count = 4 #count variable 
+for i in range(4): #for loop that iterates four times
+    for j in range(count): #for loop that iterates through the range of count
+        print('*', end=' ') #prints * in line
+    print() #prints new line
+    count -= 1 #subtracts 1 from count
