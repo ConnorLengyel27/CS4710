a26c209495e8d66c553710c33a5a30fdfed8548e
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-02-04 15:51:31 -0600

    Create Question3.py

---
+++ Assignment 2/Question3.py
@@ -0,0 +1,4 @@
+x = [23, "Python", 23.98] #list storing different data
+print(x) #prints the list
+for i in x: #for loop that iterates through the loop and prints the type of each variable
+    print(type(i))
