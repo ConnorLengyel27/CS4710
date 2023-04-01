91d90054460a73472c6e339a89924465ca4cfcd1
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-04-01 16:27:23 -0500

    Update Question1b.py

--- Assignment 3/Question1b.py
+++ Assignment 3/Question1b.py
@@ -2,7 +2,7 @@ import numpy
 from numpy.linalg import eig
 
 list = [3, -2, 1, 0]
-vector = numpy.array(list).reshape(2,2)
-eigenvalues,eigenvectors=eig(vector)
+arr = numpy.array(list).reshape(2,2)
+eigenvalues,eigenvectors=eig(arr)
 print('Eigenvalue:', eigenvalues)
 print('Eigenvector', eigenvectors)
