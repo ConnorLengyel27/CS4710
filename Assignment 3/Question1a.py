91b539aeb34c35237e0704f5433f49ab79c33680
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-04-01 18:24:47 -0500

    Update Question1a.py

--- Assignment 3/Question1a.py
+++ Assignment 3/Question1a.py
@@ -7,7 +7,6 @@ print("Original vector after reshape")
 print(vector)
 print("Shape of vector")
 print(numpy.shape(vector))
-#numpy.where(numpy.isin(vector,max), 0, vector)
 maxNum = numpy.amax(vector, axis=1)
 vector[vector == maxNum[:, None]] = 0
 print("Vector after replacing max with 0")
