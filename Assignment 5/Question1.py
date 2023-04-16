cbd3c44d8b88a6d21c11b4b893621b92d52ad826
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-04-15 19:37:06 -0500

    Update Question1.py

--- Assignment 5/Question1.py
+++ Assignment 5/Question1.py
@@ -26,6 +26,14 @@ nclusters = 3 # this is the k in kmeans
 km = KMeans(n_clusters=nclusters)
 km.fit(x)
 
+# Apply transform to both the training set and the test set.
+x_scaler = scaler.transform(x)
+pca = PCA(2)
+x_pca = pca.fit_transform(x_scaler)
+df2 = pd.DataFrame(data=x_pca)
+finaldf = pd.concat([df2,dataset[['TENURE']]],axis=1)
+print(finaldf)
+
 # predict the cluster for each data point
 y_cluster_kmeans = km.predict(x)
 from sklearn import metrics
