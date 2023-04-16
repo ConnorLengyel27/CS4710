8cf1856d6dd447c63b0131f58c383ea4cef5e8d2
Connor Lengyel <77285088+ConnorLengyel27@users.noreply.github.com>
2023-04-15 19:34:45 -0500

    Create Question1.py

---
+++ Assignment 5/Question1.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="white", color_codes=True)
+import warnings
+warnings.filterwarnings("ignore")
+
+dataset = pd.read_csv('CC GENERAL.csv')
+x = dataset.iloc[:,[1,2,3,4]]
+y = dataset.iloc[:,-1]
+print(x.shape, y.shape)
+# see how many samples we have of each tenure
+print(dataset["TENURE"].value_counts())
+
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+scaler.fit(x)
+X_scaled_array = scaler.transform(x)
+X_scaled = pd.DataFrame(X_scaled_array, columns = x.columns)
+
+# apply kmeans algorithm
+from sklearn.cluster import KMeans
+nclusters = 3 # this is the k in kmeans
+km = KMeans(n_clusters=nclusters)
+km.fit(x)
+
+# predict the cluster for each data point
+y_cluster_kmeans = km.predict(x)
+from sklearn import metrics
+score = metrics.silhouette_score(x, y_cluster_kmeans)
+print(score)
